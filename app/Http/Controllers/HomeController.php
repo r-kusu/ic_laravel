78cf291b366c19591f52fa2d31ac8b93089ceb87
mapple5 <masayo.harano@gmail.com>
2021-12-22 22:47:06 +0900

    Homecontroller微調整

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -14,7 +15,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
     /**
@@ -26,9 +27,9 @@ class HomeController extends Controller
     public function index()
     {
         // // categoriesテーブルからkey,valueを$categoriesに格納
-        // $categories=DB::table('categories')
-        // ->select('key','value')
-        // ->get();
+        $categories=DB::table('categories')
+        ->select('key','value')
+        ->get();
 
         // viewを返す(compactでviewに$categoriesを渡す)
         return view('register/index',compact('categories'));
