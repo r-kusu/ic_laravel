e9de5f9942dd6b640d600ae10dd5e06fd2dc1d46
r-kusu <ryumaexile@gmail.com>
2022-01-03 03:05:11 +0900

    データベース作成_カラム修正（カラム place の順番変更）

--- database/migrations/2022_01_03_030125_add_place_to_items_table.php
+++ database/migrations/2022_01_03_030125_add_place_to_items_table.php
@@ -14,7 +14,8 @@ class AddPlaceToItemsTable extends Migration
     public function up()
     {
         Schema::table('items', function (Blueprint $table) {
-            $table->string('place');
+            $table->string('place')->after('image_name');
+            //
         });
     }
 
@@ -26,7 +27,6 @@ class AddPlaceToItemsTable extends Migration
     public function down()
     {
         Schema::table('items', function (Blueprint $table) {
-            
             //
         });
     }
