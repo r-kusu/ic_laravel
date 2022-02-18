3a3186df383b2db0c1a1f152aa55599b4e89785c
knk-ayumi-Ste <rts.neochi@gmail.com>
2022-02-18 21:59:08 +0900

    before merge master

--- config/app.php
+++ config/app.php
@@ -232,7 +232,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'ItemSearch' => \App\Facades\ItemSearch::class,
+//        'ItemSearch' => \App\Facades\ItemSearch::class,
 
     ],
 
