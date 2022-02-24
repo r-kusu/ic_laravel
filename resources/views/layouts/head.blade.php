cbbee9ebd82bd768e3ee9aae5d178216f53347b8
knk-ayumi-Ste <rts.neochi@gmail.com>
2022-02-24 21:57:47 +0900

    アイテム一覧css修正

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -17,7 +17,6 @@
 <!-- Styles -->
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
-<link rel="stylesheet" href="{{ asset('css/style.css') }}">
 <!-- add bs-suggest css for the tag suggest functionality, Jan. 28 2022 by K -->
 <link rel="stylesheet" href="{{ asset('css/bootstrap-suggest.css') }}">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
--- resources/views/register/shortagelist.blade.php
+++ resources/views/register/shortagelist.blade.php
@@ -9,14 +9,14 @@
 @section('content')
 
 @foreach($shortageitems as $shortageitem)
-<div class="card m-2 d-inline-block" style="min-width: 350px;max-width: 350px">
+<div class="item-card card m-2 d-inline-block">
     <div class="row g-0">
         <div class="col-md-4">
             <img src="{{$shortageitem->image_name}}" alt="アイテム画像">
         </div>
         <div class="col-md-8">
             <div class="card-body">
-                <h5 class="card-title">{{$shortageitem->name}}</h5>
+                <h5 class="item-name card-title">{{$shortageitem->name}}</h5>
                 <div class="card-text">残り{{$shortageitem->stock}}個</div>
                 <a class="btn btn-danger" href="{{ url('/editdaily/{item_id}') }}" role="button">変更</a>
             </div>
