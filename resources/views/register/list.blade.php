68316ea501f3235a69818fe9b1cef2342894eba7
mapple5 <masayo.harano@gmail.com>
2022-03-13 23:27:57 +0900

    画像が無ければNO IMAGE！完了

--- resources/views/register/list.blade.php
+++ resources/views/register/list.blade.php
@@ -8,17 +8,22 @@
 
 @section('content')
 
-@foreach($listitem as $listitem)
+@foreach($listitem as $li)
 <div class="card mb-3" style="max-width: 400px;">
     <div class="row g-0">
         <div class="col-md-4">
-            <img src="{{$listitem->image_name}}" alt="アイテム画像" width="80px">
+            @if($li->image_name === null)
+            <img src="{{ asset('img/no_image_logo.png')}}"alt="アイテム画像" width="80px">
+            
+            @else
+                <img src="{{$li->image_name}}" alt="アイテム画像" width="80px">
+            @endif
         </div>
         <div class="col-md-8">
             <div class="card-body">
-                <h5 class="card-title">{{$listitem->name}}</h5>
-                <div class="card-text">残り{{$listitem->stock}}個</div>
-                <a class="btn btn-danger" href="{{ url('/editdaily/' . $listitem->id) }}" role="button">変更</a>
+                <h5 class="card-title">{{$li->name}}</h5>
+                <div class="card-text">残り{{$li->stock}}個</div>
+                <a class="btn btn-danger" href="{{ url('/editdaily/' . $li->id) }}" role="button">変更</a>
         </div>
     </div>
 </div>
