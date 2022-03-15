c5e9e50ca1a8e77565590217b9043c0f9bf35d0a
knk-ayumi-Ste <rts.neochi@gmail.com>
2022-03-15 20:56:28 +0900

    after merge from mapple5

--- resources/views/register/list.blade.php
+++ resources/views/register/list.blade.php
@@ -8,21 +8,21 @@
 
 @section('content')
 
-@foreach($listitem as $listitem)
+@foreach($listitem as $item)
 <div class="item-card card m-2 d-inline-block">
     <div class="row g-0">
         <div class="col-md-4">
-            @if($li->image_name === null)
+            @if($item->image_name === null)
             <img src="{{ asset('img/no_image_logo.png')}}"alt="アイテム画像" width="80px">
             
             @else
-                <img src="{{$li->image_name}}" alt="アイテム画像" width="80px">
+                <img src="{{$item->image_name}}" alt="アイテム画像" width="80px">
             @endif
         </div>
         <div class="col-md-8">
             <div class="card-body">
-                <h5 class="item-name card-title">{{$listitem->name}}</h5>
-                <div class="card-text">残り{{$listitem->stock}}個</div>
+                <h5 class="item-name card-title">{{$item->name}}</h5>
+                <div class="card-text">残り{{$item->stock}}個</div>
                 <a class="btn btn-danger" href="{{ url('/editdaily/'.$item->id) }}" role="button">変更</a>
             </div>
         </div>
